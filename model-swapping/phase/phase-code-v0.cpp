34f98d3ae1af63d1be6c2d9d8103a0a7beb64375
Ryan Thomas Lynch <hello@ryanlyn.ch>
2021-09-22 15:46:10 -0400

    updated license for phase detection code
    added Apache 2.0 license and attributed original python code source.

--- model-swapping/phase/phase-code-v0.cpp
+++ model-swapping/phase/phase-code-v0.cpp
@@ -1,8 +1,27 @@
+// # Copyright (c) 2021, Georgia Institute of Technology
+// #
+// # SPDX-License-Identifier: Apache-2.0
+// #
+// # Licensed under the Apache License, Version 2.0 (the "License");
+// # you may not use this file except in compliance with the License.
+// # You may obtain a copy of the License at
+// #
+// #     http://www.apache.org/licenses/LICENSE-2.0
+// #
+// # Unless required by applicable law or agreed to in writing, software
+// # distributed under the License is distributed on an "AS IS" BASIS,
+// # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// # See the License for the specific language governing permissions and
+// # limitations under the License.
+
+
 // Ryan Thomas Lynch
 // Georgia Institute of Technology
 // ryan.lynch@gatech.edu
 // CRNCH Lab
-// based on pseudocode from https://doi.org/10.1145/3457388.3458670 and Python code from Patrick Lavin
+// based on pseudocode from https://doi.org/10.1145/3457388.3458670
+// and Python code from Patrick Lavin available at:
+// https://github.com/plavin/ModelSwapping/blob/master/PhaseDetector.py
 
 #include <cstdio>
 #include <cinttypes>
